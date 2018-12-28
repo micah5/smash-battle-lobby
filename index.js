4f82f7ae17f905f09ff89e5f471b71c273179c88
micah price <98mprice@gmail.com>
2018-12-28 20:36:01 +0200

    added rest api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser')
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,16 +12,29 @@ app.get('/', function(req, res){
 
 var messages = []
 
+app.get('/messages', function(req, res){
+  res.send(JSON.stringify(messages));
+});
+
+app.post('/message', function(req, res) {
+  incoming_message(req.body)
+  res.send(JSON.stringify({ success: true }));
+});
+
+function incoming_message(message) {
+  if (message.id == null && message.chat == null) {
+    console.log('send error message')
+    //todo: send error message
+  } else {
+    message['date'] = new Date(Date.now()).toLocaleString()
+    messages.push(message)
+    io.emit('chat message', message);
+  }
+}
+
 io.on('connection', function(socket){
   socket.on('chat message', function(message){
-    if (message.id == null && message.chat == null) {
-      console.log('send error message')
-      //todo: send error message
-    } else {
-      message['date'] = new Date(Date.now()).toLocaleString()
-      messages.push(message)
-      io.emit('chat message', message);
-    }
+    incoming_message(message)
   });
   socket.on('init', function() {
     io.emit('init', messages);
